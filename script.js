6c6cfd230c2cba0461c15c0e550dea0c47f7ff30
Prem Bhati <prem.bhati@xebia.com>
2024-11-12 13:06:00 +0530

    first commit

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+
+
+
+
+
+$('.owl-carousel').owlCarousel({
+  oop:true,
+  margin:10,
+  autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+
+
